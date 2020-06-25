577b4b437b8723d7d0bb24e554e4c06398b2aed6
akhanji1964 <44018433+akhanji1964@users.noreply.github.com>
2020-06-25 09:46:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aws_ses_service",
-    version="0.0.7",
+    version="0.0.8",
     author="Quaking Aspen",
     author_email="info@quakingaspen.net",
     description="This library is to help send emails using AWS ses service",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Quakingaspen-codehub/aws_ses_service",
     packages=["aws_ses_service"],
-    install_requires=["aws-s3-resource==0.0.4", "boto3==1.13.15"],
+    install_requires=["aws-s3-resource==0.0.7", "boto3==1.13.15"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
