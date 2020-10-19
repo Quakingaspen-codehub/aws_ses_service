5a7456296eddb1ca83615cc52bee261607617f4b
feras <feras@quakingaspen.net>
2020-10-19 15:17:31 +0300

    support single and broadcast send email

--- aws_ses_service/__init__.py
+++ aws_ses_service/__init__.py
@@ -1,13 +1,15 @@
 import boto3
 import os
 from email.mime.multipart import MIMEMultipart
+import email.utils as EmailUtils
 
 
 class Email:
-    client = boto3.client('ses',region_name=os.environ['AWS_REGION_NAME'])
+    client = boto3.client('ses', region_name=os.environ['AWS_REGION_NAME'])
 
-    def __init__(self, sender, receiver, subject, body=list(), attachments=list()):
-        self.sender = sender
+    def __init__(self, sender_email, sender_name, receiver, subject, body=list(), attachments=list()):
+        self.sender_email = sender_email
+        self.sender_name = sender_name
         self.receiver = receiver
         self.subject = subject
         self.body = body
@@ -20,13 +22,19 @@ class Email:
         self.setup_email_attachments(email)
 
         # Send
-        Email.client.send_raw_email(Source=self.sender, Destinations=self.receiver,
+        Email.client.send_raw_email(Source=self.sender_email, Destinations=self.receiver,
                                     RawMessage={'Data': email.as_string()})
 
     def create_the_email(self):
+
         email = MIMEMultipart('mixed')
         email['Subject'] = self.subject
-        email['From'] = self.sender
+        email['From'] = EmailUtils.formataddr((self.sender_name,
+                                               self.sender_email))
+
+        # email = MIMEMultipart('mixed')
+        # email['Subject'] = self.subject
+        # email['From'] = self.sender
 
         return email
 
@@ -41,6 +49,20 @@ class Email:
     def setup_email_attachments(self, email):
         for attachment in self.attachments:
             email.attach(attachment.get_attachment_object())
+
+
+class SingleEmail(Email):
+
+    def create_the_email(self):
+        email = super().create_the_email()
+
+        email['To'] = self.receiver[0]
+
+        return email
+
+
+class BroadcastEmail(Email):
+    pass
 
 # from aws_ses_service import Email
 # from aws_ses_service.body import EmailTextBody
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aws_ses_service",
-    version="0.0.9",
+    version="0.0.10",
     author="Quaking Aspen",
     author_email="info@quakingaspen.net",
     description="This library is to help send emails using AWS ses service",
